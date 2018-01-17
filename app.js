d2bc2500e0f65db63a88daafb71147ce59c2529d
Luis Perez <lperez@belatrixsf.com>
2018-01-17 17:12:00 -0500

    UPDATED API for users route

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+
 // Routes
 const productRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/orders')
+const userRoutes = require('./api/routes/users')
 
 const app = express();
 
@@ -18,6 +20,7 @@ app.use(bodyParser.json())
 // Routes which should handle request
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/users', userRoutes);
 
 /*
 app.use((request, response, next) => {
