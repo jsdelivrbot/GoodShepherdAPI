7eb7bf925179ccb4d8684d6ff9f78ff0c11973c9
Luis Perez <luisperez.r@icloud.com>
2018-02-15 07:32:59 -0500

    UPDATED logic

--- api/routes/api.js
+++ api/routes/api.js
@@ -13,7 +13,6 @@ const Person = require('../models/person');
 
 
 
-
 // Authentication Method
 
 
--- app.js
+++ app.js
@@ -8,8 +8,10 @@ const app = express();
 const apiRoutes = require('./api/routes/api')
 
 
+mongoose.connect("mongodb://luisperez:master@ds133547.mlab.com:33547/goodshepherd");
+//mongoose.connect("mongodb://lpramirez:Xtomili2413!@democluster-shard-00-00-q6ij8.mongodb.net:27017,democluster-shard-00-01-q6ij8.mongodb.net:27017,democluster-shard-00-02-q6ij8.mongodb.net:27017/test?ssl=true&replicaSet=DemoCluster-shard-0&authSource=admin");
 
-mongoose.connect("mongodb://lpramirez:Xtomili2413!@democluster-shard-00-00-q6ij8.mongodb.net:27017,democluster-shard-00-01-q6ij8.mongodb.net:27017,democluster-shard-00-02-q6ij8.mongodb.net:27017/test?ssl=true&replicaSet=DemoCluster-shard-0&authSource=admin");
+
 
 app.use(morgan('dev')); // request logger
 app.use(bodyParser.urlencoded({extended: false}));
@@ -18,9 +20,7 @@ app.use(bodyParser.json())
 
 
 app.use('/api', apiRoutes);
-//require('./api/routes/api')(app);
 require('./api/routes/user.route')(app);
-
 
 
 module.exports = app
